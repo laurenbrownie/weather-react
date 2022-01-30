1973b9050eb486491a6efbb74fdec0f0c6ae4e6f
laurenbrownie <95157266+laurenbrownie@users.noreply.github.com>
2022-01-29 23:24:25 -0500

    add link to source code

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import ReactDOM from "react-dom";
 
 function App() {
   return (
-    <div>
+    <div className="app-aesthetic">
       <h1>this is a weather app</h1>
       <Weather />
+      <br />
+      <br />
+      <a href="https://https://github.com/laurenbrownie/weather-react">
+        view source code
+      </a>
     </div>
   );
 }
